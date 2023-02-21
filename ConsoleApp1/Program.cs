ad06b1a37c7c3a697f2334ee6a7100c657dd7c85
GidJoe <winterm@bfwob.de>
2023-02-21 20:42:14 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿String Name, Haustier;
+int alter;
+
+Console.WriteLine("Hello World, dies ist mein erste C# Programm");
+Console.WriteLine("Sag mir dein Alter");
+alter = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sag mir deinen Namen");
+Name = Console.ReadLine();
+
+Console.WriteLine("Sag mir den Namen deines Haustier");
+Haustier = Console.ReadLine();
+
+
+Console.WriteLine("Dein Name lautet " + Name + ". Dein Haustier heißt " + Haustier + " und du bist " + alter);
+
+
+
+
